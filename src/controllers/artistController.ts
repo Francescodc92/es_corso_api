cb629388768fd0d1ad91eb2a682d6ed01543cf39
Francesco Di Corpo <96888350+Francescodc92@users.noreply.github.com>
2025-05-05 14:44:13 +0200

    add update artist function

--- src/controllers/artistController.ts
+++ src/controllers/artistController.ts
@@ -2,80 +2,142 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
-export const getArtists = async (req:FastifyRequest, reply:FastifyReply) => {
-    try {
-        const artists = await prisma.artist.findMany();
-        return reply.status(200).send(artists);
-    }catch (error) {
-        return reply.status(500).send({ error: "Internal Server Error" });
-    }
-}
+export const getArtists = async (req: FastifyRequest, reply: FastifyReply) => {
+  try {
+    const artists = await prisma.artist.findMany();
+    return reply.status(200).send(artists);
+  } catch (error) {
+    return reply.status(500).send({ error: "Internal Server Error" });
+  }
+};
 
-export const createArtist = async (req:FastifyRequest, reply:FastifyReply) => {
-    const createArtistSchema = z.object({
-        name: z.string().min(3),
+export const createArtist = async (
+  req: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const createArtistSchema = z.object({
+    name: z.string().min(3),
+  });
+
+  try {
+    const { name } = createArtistSchema.parse(req.body);
+
+    const artistAlreadyExists = await prisma.artist.findUnique({
+      where: {
+        name,
+      },
     });
 
-    try {
-        const {name} = createArtistSchema.parse(req.body);
-
-        const artistAlreadyExists = await prisma.artist.findUnique({
-            where: {
-                name,
-            },
-        })
-
-        if (artistAlreadyExists) {
-            return reply.status(400).send({ error: "Artist already exists" });
-        }
-        
-        const artist = await prisma.artist.create({
-            data: {
-                name,
-            },
-        });
-        
-        return reply.status(201).send(artist);
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return reply.status(400).send({
-                message:'Error during validation',
-                error:error.flatten().fieldErrors
-            });
-        }
-        
-        return reply.status(500).send({ error: "Internal Server Error" });
+    if (artistAlreadyExists) {
+      return reply.status(400).send({ error: "Artist already exists" });
     }
-}
 
-export const getArtistById = async (req:FastifyRequest, reply:FastifyReply) => {
-    const getArtistByIdSchema = z.object({
-        id: z.string().uuid(),
+    const artist = await prisma.artist.create({
+      data: {
+        name,
+      },
     });
 
-    try {
-        const { id } = getArtistByIdSchema.parse(req.params);
-
-        const artist = await prisma.artist.findUnique({
-            where: {
-                id,
-            },
-        });
-
-        if (!artist) {
-            return reply.status(404).send({ error: "Artist not found" });
-        }
-
-        return reply.status(200).send(artist);
-    }catch (error) {
-
-        if (error instanceof z.ZodError) {
-            return reply.status(400).send({
-                message:'Error during validation',
-                error:error.flatten().fieldErrors
-            });
-        }
-
-        return reply.status(500).send({ error: "Internal Server Error" });
+    return reply.status(201).send(artist);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return reply.status(400).send({
+        message: "Error during validation",
+        error: error.flatten().fieldErrors,
+      });
     }
-}
+
+    return reply.status(500).send({ error: "Internal Server Error" });
+  }
+};
+
+export const getArtistById = async (
+  req: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const getArtistByIdSchema = z.object({
+    id: z.string().uuid(),
+  });
+
+  try {
+    const { id } = getArtistByIdSchema.parse(req.params);
+
+    const artist = await prisma.artist.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!artist) {
+      return reply.status(404).send({ error: "Artist not found" });
+    }
+
+    return reply.status(200).send(artist);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return reply.status(400).send({
+        message: "Error during validation",
+        error: error.flatten().fieldErrors,
+      });
+    }
+
+    return reply.status(500).send({ error: "Internal Server Error" });
+  }
+};
+
+export const updateArtist = async (
+  req: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const updateArtistSchema = z.object({
+    id: z.string().uuid(),
+  });
+
+  const updateArtistBodySchema = z.object({
+    name: z.string().min(3),
+  });
+
+  try {
+    const { id } = updateArtistSchema.parse(req.params);
+    const { name } = updateArtistBodySchema.parse(req.body);
+
+    const artist = await prisma.artist.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!artist) {
+      return reply.status(404).send({ error: "Artist not found" });
+    }
+
+    const artistAlreadyExists = await prisma.artist.findUnique({
+      where: {
+        name,
+      },
+    });
+    if (artistAlreadyExists && artistAlreadyExists.id !== id) {
+      return reply.status(400).send({ error: "Artist already exists" });
+    }
+
+    const updatedArtist = await prisma.artist.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return reply.status(200).send(updatedArtist);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return reply.status(400).send({
+        message: "Error during validation",
+        error: error.flatten().fieldErrors,
+      });
+    }
+
+    return reply.status(500).send({ error: "Internal Server Error" });
+  }
+};
--- src/routes/artistRoutes.ts
+++ src/routes/artistRoutes.ts
@@ -1,14 +1,19 @@
 import { FastifyInstance } from "fastify";
-import { createArtist, getArtistById, getArtists } from "../controllers/artistController";
+import {
+  createArtist,
+  getArtistById,
+  getArtists,
+  updateArtist,
+} from "../controllers/artistController";
 
 export const artistsRoutes = async (app: FastifyInstance) => {
-    app.get("/", getArtists)
+  app.get("/", getArtists);
 
-    app.get('/:id', getArtistById);
-    
-    app.post('/', createArtist);
-    
-    app.put('/:id', () => {});
-    
-    app.delete('/:id', () => {});
-}
+  app.get("/:id", getArtistById);
+
+  app.post("/", createArtist);
+
+  app.put("/:id", updateArtist);
+
+  app.delete("/:id", () => {});
+};
